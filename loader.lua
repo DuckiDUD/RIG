7dbf3d56cb7dc6916da410aac484047ce31ec8bc
DuckiDUD <151539718+DuckiDUD@users.noreply.github.com>
2023-11-22 00:05:34 +0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -36,9 +36,9 @@ local UISTR = Instance.new("UIStroke",CIRCLE)
 UISTR.Thickness = 6
 UISTR.Color = Color3.fromRGB(255, 57, 57)
 
---Properties:
+--Properties ok:
 
-ScreenGui.Parent = game.StarterGui
+ScreenGui.Parent = game.CoreGui
 ScreenGui.IgnoreGuiInset = true
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
