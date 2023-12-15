631964aad3e96cd3fc41a4e26423a95777237115
DuckiDUD <151539718+DuckiDUD@users.noreply.github.com>
2023-12-15 21:19:16 +0400

    Update lil.lua

--- lil.lua
+++ lil.lua
@@ -1,6 +1,11 @@
 local wrsp = game:GetService("Workspace")
 for i, v in pairs(wrsp["__THINGS"].Breakables:GetChildren()) do
 	local hitbox = v:GetChildren()[1]
+	if hitbox then
+		print("found hitbox")
+	else
+		print("not found hitbox")
+	end
 	fireclickdetector(hitbox:FindFirstChild("ClickDetector"))
 	if i%10 == 0 then
 		task.wait()
