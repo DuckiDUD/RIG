a7769baf02f282abf925fd2c7a27f99eb63b4323
DuckiDUD <151539718+DuckiDUD@users.noreply.github.com>
2023-12-15 20:07:41 +0400

    Update lil.lua

--- lil.lua
+++ lil.lua
@@ -1,14 +1,8 @@
 while wait(1) do
 	local wrsp = game:GetService("Workspace")
-	for i, v in pairs(wrsp["__THINGS"].Pets:GetChildren()) do
-		local BlackList = "Highlight"
-		local Breakable = nil
-		local brks = wrsp["__THINGS"].Breakables:GetChildren()
-		repeat wait() Breakable = brks[math.random(1,#brks)] until Breakable.Name ~= BlackList
-		local Remote = game.ReplicatedStorage.Network['Pets_SetTargetBulk']
+	local Remote = game.ReplicatedStorage.Network['Pets_SetTargetBulk']
 		
-		local Arguments = {}
-		Remote:FireServer(unpack(Arguments))
-		print("Works")
-	end
+	local Arguments = {}
+	Remote:FireServer(unpack(Arguments))
+	print("Works")
 end
