8001a64c59ac24c2d96556d6f5ca6da84cf17644
DuckiDUD <151539718+DuckiDUD@users.noreply.github.com>
2023-12-15 21:33:37 +0400

    Update lil.lua

--- lil.lua
+++ lil.lua
@@ -1,8 +1,7 @@
 local wrsp = game:GetService("Workspace")
 for i, v in pairs(wrsp["__THINGS"].Breakables:GetChildren()) do
 	local hitbox = v:GetChildren()
-	for i, v in pairs(hitbox) do
-		print(v.Name)
+	for i, v2 in pairs(hitbox) do
+		fireclickdetector(v2.hitbox:FindFirstChild("ClickDetector"))
 	end
-	fireclickdetector(hitbox:FindFirstChild("ClickDetector"))
 end
